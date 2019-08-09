8d8cbd6fa57a5a6b8ead337ea6a4f7eeca846c78
Krish Patel <53786660+kxp9343@users.noreply.github.com>
2019-08-09 16:02:22 -0500

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,35 @@
+import random
+
+name = ['Mike', 'Jeff', 'Samuel']
+
+
+print("Welcome to The Forest of Igguna!")
+yourName = input("What is your name traveller?   ")
+
+#Scene One
+print("Hello, " + yourName + "!")
+print("You are in a forest.")
+print("On a mission to save the world!")
+print("You will be with our secret agent John.")
+print("Your job is to find the 3 artifacts that will help you defeat our enemy")
+print("His name is Tim")
+print("His cooptation called DemiStruct is trying to transform the Earth by killing evreyone in the world")
+print("The last artifact is in his secret layer")
+print("Are you ready to start your journey?")
+first = input("Type Yes to Start No to quit")
+
+#Scene Two
+if first == "Yes":
+	print("John: We must start now as the more time we waste highers our faluire of saving the world")
+	print("John: We will start in 10 minutes")
+	print("John: So get your stuff packed and readt for our long adventure")
+second = input("Type Next to continue")
+
+if first 
+#Scene Three
+if second == "Next":
+	print("We have already been found by Tim's spy agents")
+	print("We will either have to ")
+
+
+
